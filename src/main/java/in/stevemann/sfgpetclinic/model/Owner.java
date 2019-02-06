f256ae052d0eecaf2456dbfd4086459e9f52bf16
Steve Mann <stevemann2705@gmail.com>
2019-02-06 22:15:47 +0530

    Adding POJOs

---
+++ src/main/java/in/stevemann/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package in.stevemann.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/in/stevemann/sfgpetclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package in.stevemann.sfgpetclinic.model;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
