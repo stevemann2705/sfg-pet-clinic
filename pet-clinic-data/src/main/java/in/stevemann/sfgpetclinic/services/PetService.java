853900787064ca8804da18498928d5c98cf227f9
Steve Mann <stevemann2705@gmail.com>
2019-02-08 15:18:25 +0530

    Refactoring to Common CRUD Interface. Closes #11

--- pet-clinic-data/src/main/java/in/stevemann/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/in/stevemann/sfgpetclinic/services/PetService.java
@@ -2,12 +2,6 @@ package in.stevemann.sfgpetclinic.services;
 
 import in.stevemann.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
