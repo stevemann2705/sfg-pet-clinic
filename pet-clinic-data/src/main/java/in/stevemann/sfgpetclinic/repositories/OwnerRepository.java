4c278c6bad9e0a7c0443a56e3687c6f0c957f7f3
Steve Mann <stevemann2705@gmail.com>
2019-02-20 15:55:23 +0530

    Creating Spring Data Service for Owners. Closes #40

--- pet-clinic-data/src/main/java/in/stevemann/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/in/stevemann/sfgpetclinic/repositories/OwnerRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
