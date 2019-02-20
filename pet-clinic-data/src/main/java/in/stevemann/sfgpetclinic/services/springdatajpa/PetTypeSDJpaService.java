59dac5dc54232ac0a8c0a648714110713c7ef14b
Steve Mann <stevemann2705@gmail.com>
2019-02-20 16:05:00 +0530

    Silly Typo

--- pet-clinic-data/src/main/java/in/stevemann/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
+++ pet-clinic-data/src/main/java/in/stevemann/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -11,7 +11,7 @@ import java.util.Set;
 
 @Service
 @Profile("springdatajpa")
-public class PetTypeSDJpaService extends PetTypeService {
+public class PetTypeSDJpaService implements PetTypeService {
 
     private final PetTypeRepository petTypeRepository;
 
